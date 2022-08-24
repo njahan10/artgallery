8f27b1c2b56bcf0ab3812df6c67964c9e78f501d
njahan10 <njahan10.ctg@gmail.com>
2022-08-24 15:17:17 -0600

    project created

---
+++ src/test/java/org/comit/project/ArtgalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.comit.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtgalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
